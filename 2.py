17e65296db4ab963d26867dd6130d933734deb69
Ankita karn <91003709+Karnankita04@users.noreply.github.com>
2022-08-27 14:09:57 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,110 @@
+# 1. Write a python script to add comments and print “Learning Python” on screen.
+# code:
+# I am adding this as a Comment
+# print("\"Learning Python\"")
+
+# next>----------------------------------------------------------------
+
+# 2. Write a python script to add multi line comments and print values of four variables,
+# each in a new line. Variable contains any values.
+# code:
+"""
+This is a multiline comment
+And here is the second line
+"""
+# var1="Ankita"
+# var2=13
+# var3=4.5
+# var4=True
+# print(var1,var2,var3,var4,sep="\n")
+
+# next>-------------------------------------------------------------------
+
+# 3. Write a python script to print types of variables. Create 5 variables each of them
+# containing different types of data. (like 35, True, “MySirG”,5.46, 3+4j, etc)
+# code:
+# a=12
+# b="Ankita"
+# c=5.6
+# d=3+4j
+# e=False
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+
+# next>----------------------------------------------------------------------
+
+# 4. Write a python script to print the id of two variables containing the same integer
+# values.
+# code:
+# a=5
+# b=5 
+# print(id(a))
+# print(id(b))
+
+# next>-----------------------------------------------------------------------
+
+# 5. Create four variables in a Python script and assign values of different data types to
+# them. Write a Python script to print value, its type and id of each variable
+# code:
+
+# a=12
+# b="Ankita"
+# c=5.6
+# d=3+4j
+# e=False
+# print(a,type(a),id(a))
+# print(b,type(b),id(b))
+# print(c,type(c),id(c))
+# print(d,type(d),id(d))
+# print(e,type(e),id(e))
+
+# next>--------------------------------------------------------------------------
+
+# 6. Write a python script to print all the keywords
+# code:
+
+# import keyword
+# print(keyword.kwlist)
+
+# next>--------------------------------------------------------------------------
+
+# 7. On Python shell use help() function and display the list of keywords
+# code:
+# help()
+# help> keywords
+
+# next>--------------------------------------------------------------------------
+
+
+# 8. Create two Python files A0.py and A1.py. Create a variable in A1.py and assign some
+# value to it. Write a python script to import A1 module in A0 and print value of the
+# variable created in A0.py
+# code:
+# import A1
+# print(A1.a)
+
+# next>---------------------------------------------------------------------------
+# 9. Name the keywords, used as data in the Python script.
+
+# True,False,None
+
+# next>---------------------------------------------------------------------------
+
+# 10. Write a python script to display the current date and time. First create variables to
+# store date and time, then display date and time in proper format (like: 13-8-2022 and 9:00 PM)
+# code :
+# from datetime import datetime
+# dt = datetime.today() ;
+# d1 = dt.strftime("%d-%m-%Y and %I:%M %p")
+# print(d1)
+
+
+
+
+
+
+
+
